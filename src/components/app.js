18cb4ffefcc24803db7d4b0bf304a517e5a38a51
Scott Bowler <scott.bowler@learningfuze.com>
2019-02-11 14:45:12 -0800

    Completed add student form and delete student

--- src/components/app.js
+++ src/components/app.js
@@ -1,24 +1,70 @@
 import 'materialize-css/dist/css/materialize.min.css';
 import 'materialize-css/dist/js/materialize.min';
 import '../assets/css/app.scss';
-import React from 'react';
+import React, { Component } from 'react';
 import AddStudent from './add_student';
 import Table from './table';
+import studentData from '../data/get_all_students';
+import { randomString } from '../helpers';
 
+class App extends Component {
+    state = {
+        students: []
+    }
 
-const App = () => (
-    <div>
-        <h1 className="center">SGT</h1>
+    componentDidMount(){
+        this.getStudentData();
+    }
 
-        <div className="row">
-            <div className="col s12 m8">
-                <Table/>
+    deleteStudent = (id) => {
+        const indexToDelete = this.state.students.findIndex((student) => {
+            return student.id === id;
+        });
+
+        if(indexToDelete >= 0){
+            const tempStudents = this.state.students.slice();
+
+            tempStudents.splice(indexToDelete, 1);
+
+            this.setState({
+                students: tempStudents
+            });
+        }
+    }
+
+    addStudent = (student) => {
+
+        student.id = randomString();
+
+        this.setState({
+            students: [...this.state.students, student]
+        });
+    }
+
+    getStudentData(){
+        // Call server to get student data
+
+        this.setState({
+            students: studentData
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <h1 className="center">SGT</h1>
+
+                <div className="row">
+                    <div className="col s12 m8">
+                        <Table deleteStudent={this.deleteStudent} studentList={this.state.students}/>
+                    </div>
+                    <div className="col s12 m4">
+                        <AddStudent add={this.addStudent}/>
+                    </div>
+                </div>
             </div>
-            <div className="col s12 m4">
-                <AddStudent/>
-            </div>
-        </div>
-    </div>
-);
+        );
+    }
+}
 
 export default App;
